9fe585c5f72340825b3ce1251164db62ef7b016c
Liam Cryan <reliability.data@gmail.com>
2018-01-13 17:01:11 -0800

    flask app to serve pdf files from a repository

---
+++ config.py
@@ -0,0 +1,6 @@
+
+# choose the target directory of the files you want to display, ex:  "/Users/liamcryan/Repository"
+STARTING_DIRECTORY = "data"
+
+# is really only meant for pdf, don't change this
+FILE_TYPES = [".pdf"]
